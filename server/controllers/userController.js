fcbd80761aff5641cd5e6a53e77d4329f5fd73e7
Alan Budiarto <alanwbudiarto@gmail.com>
2020-04-06 09:46:04 +0700

    First commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,10 @@
+const { User } = require('../models');
+
+class UserController {
+    static register(req, res, next) {
+        const { email, password } = req.body;
+
+    }
+}
+
+module.exports = UserController;
---
+++ server/middlewares/encrypt.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv').config();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
